1a4ca00d10d24fd969b765aafb364d5a0648c6bf
Hades62442 <83868670+Hades62442@users.noreply.github.com>
2022-07-12 14:54:49 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+def AND(a,b):
+    return (a and b)
+
+def OR(a,b):
+    return (a or b)
+
+def XOR(a,b):
+    return (a!=b)
+
+def NOT(a):
+    return (not a)
+
+def NAND(a,b):
+    return not (a and b)
+
+def NOR(a,b):
+    return not (a or b)
+
+def XNOR(a,b):
+    return (a == b)
+
+
+def test(a,b):
+    print(f"{AND(a,b)}\t{OR(a,b)}\t{XOR(a,b)}\t{NAND(a,b)}\t{NOR(a,b)}\t{XNOR(a,b)}")
+
+a = False
+b = False
+test(a,b)
+b = True
+test(a,b)
+a = True
+b = False
+test(a,b)
+b = True
+test(a,b)
